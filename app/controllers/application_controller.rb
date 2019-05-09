57f3a552756f2e0b35d3969bb44521ea56af12ba
Adriaan Beiertz <adriaanbd@gmail.com>
2019-05-09 12:50:37 -0500

    Started user authentication, not finished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :authorize
+
+  protected 
+
+    def authorize
+      unless User.find_by(email: session[:email])
+        redirect_to login_url, notice: 'Please log in'
+      end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  controller :sessions do 
+    get 'login' => :new 
+    post 'login' => :create 
+    delete 'logout' => :destroy 
+  end
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
