bd27f7ba69193bbc2a02cb9c23bfcb8abc7c6546
mcrd25 <mayacrd25@gmail.com>
2019-05-10 18:02:29 -0400

    Implemented rememberme functionality with Digest class as instructed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,46 +1,41 @@
 class User < ApplicationRecord
 	attr_accessor :remember_token
 
-	before_create :remember
+	before_create :set_token
   before_save { self.email = email.downcase }
   validates :name, presence: true 
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
   has_secure_password
 
-  class << self
-    # Returns the hash digest of the given string.
-    def digest(string)
-      # activemodel/lib/active_model/secure_password.rb
-      # SecurePassword.min_cost is false by default
-      cost = BCrypt::Engine::MIN_COST if ActiveModel::SecurePassword.min_cost
-      cost ||= BCrypt::Engine.cost
-
-      BCrypt::Password.create(string, cost: cost)
-    end
-
-    # Returns a random token.
-    def new_token
-      SecureRandom.urlsafe_base64
-    end
+  def User.digest(string)
+    Digest::SHA1.hexdigest(string)
   end
 
-  # Remembers a user in the database for use in persistent sessions.
+  def generate_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def set_token
+  	self.remember_token = generate_token
+  	self.remember_digest = Digest::SHA1.hexdigest(remember_token)
+  end
+
   def remember
-    self.remember_token = User.new_token
+    self.remember_token = generate_token
     # updates a single attribute & saves record (doesn't validate):
     # https://apidock.com/rails/ActiveRecord/Persistence/update_attribute
-    update_attribute(:remember_digest, User.digest(remember_token))
+    update_attribute(:remember_digest, Digest::SHA1.hexdigest(remember_token))
   end
 
-  # Returns true if the given token matches the digest.
-  def authenticated?(attribute, token)
-    digest = send("#{attribute}_digest")
-    return false if digest.nil?
-    BCrypt::Password.new(digest).is_password?(token)
+  def authenticated?(token)
+    return false if remember_digest.nil?
+    self.remember_digest == Digest::SHA1.hexdigest(token)
   end
 
   def forget
     update_attribute(:remember_digest, nil)
   end
 end
+
+
